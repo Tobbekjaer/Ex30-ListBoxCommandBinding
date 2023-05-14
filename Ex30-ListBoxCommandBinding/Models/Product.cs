1b1e5f3825ba1c158417c2a7ded60f6cfc0e67c0
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-05-14 14:22:51 +0200

    Add project files.

---
+++ Ex30-ListBoxCommandBinding/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex30_ListBoxCommandBinding.Models
+{
+    public class Product
+    {
+		private string _name;
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		private double _price;
+
+		public double Price
+		{
+			get { return _price; }
+			set { _price = value; }
+		}
+
+
+
+	}
+}
